390138376ba93d832a64a98063b6d3335ba5240c
Rcano <Rcanocorbera@gmail.com>
2020-08-27 06:57:28 +0200

    Arrays

---
+++ TiposBasicos/Arrays/app.js
@@ -0,0 +1,8 @@
+var array = [1, 2, 3, 4, 5, 6, 7];
+var villanos = ["Omega Rojo", "Dormammu", "Duende Verde"];
+console.log(villanos[0]);
+console.log(villanos[0].charAt(0));
+console.log(villanos[1].charAt(0));
+console.log(villanos[1]);
+console.log(villanos[2].charAt(0));
+console.log(villanos[2]);
---
+++ TiposBasicos/Arrays/app.ts
@@ -0,0 +1,12 @@
+let array:number[] = [1,2,3,4,5,6,7]
+let villanos:string[] = ["Omega Rojo","Dormammu","Duende Verde"];
+
+console.log(villanos[0]);
+console.log(villanos[0].charAt(0));
+
+console.log(villanos[1].charAt(0));
+console.log(villanos[1]);
+
+console.log(villanos[2].charAt(0));
+console.log(villanos[2]);
+
