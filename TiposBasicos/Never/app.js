6c8ec73258fb7e1d3a1215cbb8abecde2bd9142f
Rcano <Rcanocorbera@gmail.com>
2020-08-28 14:53:13 +0200

    never

---
+++ TiposBasicos/Never/app.js
@@ -0,0 +1,4 @@
+function error(mensaje) {
+    throw new Error(mensaje);
+}
+error("Error critico... linea 11 alcanzada");
---
+++ TiposBasicos/Never/app.ts
@@ -0,0 +1,9 @@
+function error(mensaje):never{
+    throw new Error(mensaje);
+}
+
+error("Error critico... linea 11 alcanzada");
+
+
+
+
