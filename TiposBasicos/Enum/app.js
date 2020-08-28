a65de51cec6440314017b9fefd1d44fad523b5dd
Rcano <Rcanocorbera@gmail.com>
2020-08-28 13:34:49 +0200

    enum

---
+++ TiposBasicos/Enum/app.js
@@ -0,0 +1,9 @@
+var Volumen;
+(function (Volumen) {
+    Volumen[Volumen["min"] = 1] = "min";
+    Volumen[Volumen["medio"] = 5] = "medio";
+    Volumen[Volumen["max"] = 10] = "max";
+})(Volumen || (Volumen = {}));
+var audio = Volumen.min;
+console.log(audio);
+console.log(Volumen);
---
+++ TiposBasicos/Enum/app.ts
@@ -0,0 +1,11 @@
+enum Volumen{
+    min = 1,
+    medio = 5,
+    max = 10
+}
+
+let audio:number = Volumen.min;
+
+console.log(audio);
+console.log(Volumen);
+
